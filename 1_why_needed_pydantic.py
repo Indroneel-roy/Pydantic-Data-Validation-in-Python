5d0f647a96e4da0842eeeb2d11d4d979b15e244e
Indroneel Roy <indroneel1000@gmail.com>
2025-05-23 01:13:09 +0600

    Update

--- 1_why_needed_pydantic.py
+++ 1_why_needed_pydantic.py
@@ -13,7 +13,8 @@ def valid_data(data):
 # With Pydantic:
 # It handles this automatically:    
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
 
 class User(BaseModel):
     name: str
@@ -24,3 +25,27 @@ data = {'name': 'Alice', 'age': '25'}  # '25' is a string
 user = User(**data)
 print(user.age)  # it converted automatically to to int
                 
+##
+
+class Patient(BaseModel):
+    name  : Annotated[str, Field(max_length=50, title="Name of the patient", description="Type the name of the patient")]
+    age : int
+    email: EmailStr
+    linkedin_url: AnyUrl
+    weight : float = Field(gt=0)
+    married : bool = False
+    allergies : Annotated[Optional[List[str]], Field(default=None, max_length=5)]
+    contact_details : Dict[str, str]
+
+def insert_patient_data(patient : Patient):
+    print(patient.name)
+    print(patient.age)
+    print(patient.allergies)
+    # print(patient.allergies)
+    print(patient.married)
+    print("inserted")
+    
+patient_info = {"name" : "Shishir", "age" : "30", 'email':'abc@gmail.com', 'linkedin_url':'http://linkedin.com/1322', "weight" : 60.2, "married" : True, "allergies" : ["Pollen", "Dust"], 'contact_details':{'phone':'2353462'}}    
+
+patient1 = Patient(**patient_info)
+insert_patient_data(patient1)        
