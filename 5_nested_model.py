31ca3c835453ca79b152d30be8469ae1c4b54640
Indroneel Roy <indroneel1000@gmail.com>
2025-05-24 15:57:24 +0600

    nested model

---
+++ 5_nested_model.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+class address(BaseModel):
+    city : str
+    state : str
+    pin : str
+
+class Patient(BaseModel):
+    name : str
+    gender : str
+    age : int
+    address : address
+    
+address_dict = {'city': 'San Francisco', 'state': 'California', 'pin': '94103'}
+Address1 = address(**address_dict)
+patient_dict = {'name': 'Shishir', 'gender': 'male', 'age': 35, 'address': Address1}
+patient1 = Patient(**patient_dict)
+print(patient1)
+print(patient1.address.pin)
+print(patient1.address.city)
+        
